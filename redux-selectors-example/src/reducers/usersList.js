bb9077e40a7da9dbbf1ce45b9a6d7d273be2e9fd
Jonny Pickard <Jonathandpickard@gmail.com>
2018-11-11 20:42:25 +0000

    Implemented Immutable js with the delete user reducer

--- redux-selectors-example/src/reducers/usersList.js
+++ redux-selectors-example/src/reducers/usersList.js
@@ -8,10 +8,19 @@ export default (state = initialState, action) => {
     case FETCH_USERS:
       return state.mergeDeep(action.payload);
     case DELETE_USER:
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.payload.userId)
-      };
+      const userId = action.payload.userId;
+      const userKey = 'user' + userId;
+      const allIds = state.getIn(['users', 'allIds']);
+
+      // Remove the user object from the users>byId state
+      const removeUserState = state.removeIn(['users', 'byId', userKey]);
+      // Remove the user id from the allIds array
+      const removeUserIdState = removeUserState.setIn(
+        ['users', 'allIds'],
+        allIds.filter(id => id !== userId)
+      );
+
+      return removeUserIdState;
     default:
       return state;
   }
